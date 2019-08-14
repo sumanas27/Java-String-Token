31816086a865e726f1f35055bd12b21ab30f2597
Sumana Saha <34478920+sumanas27@users.noreply.github.com>
2019-08-14 12:43:55 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -8,8 +8,17 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
-        String[] splittedStrings = s.split("[ !,?.\\_'@]+");
-        System.out.println(splittedStrings.length);
+        String[] splittedStrings = s.trim().split("[ !,?.\\_'@]+");
+        //Edge cases
+        if(s == ""){
+            System.out.println("0");
+        } else if(s.length() > 400000) {
+            return;
+        } else if (s == null || s.trim().equals("")) {
+           System.out.println("0");
+        } else {
+            System.out.println(splittedStrings.length);
+        }
         Arrays.stream(splittedStrings)
                         .forEach(i -> System.out.println(i));
         scan.close();
